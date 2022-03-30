60cb898eed1283c0ead373cc927f361064cbb22b
Chase <sccala18@gmail.com>
2022-03-30 10:39:43 -0700

    clean up css

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-// @ts-nocheck
+const tabList = document.querySelector('[role="tablist"]')
+const tabs = tabList.querySelectorAll('[role="tab"]')
+tabs.forEach(tab => {
+  tab.addEventListener('click', changeTabPanel)
+})
 
-var $navitem=$
-$(function () {
-  $('.mobile-nav-toggle li').click(function () {
-e.preventDefault()
-   $()
+const navClick = $(function () {
+  $('.mobile-nav-toggle li').on('click', function () {
+    $('[role="tab"]').addClass('.active')
+    console.log(`$('.active') is now active`)
   })
 })
-$(function () {
-  $('.primary-navigation').click(function () {
-  $('.primary-navigation').add('.active')
-  })
-})
+
+function changeTabPanel(e) {
+  const targetTab = e.target
+  const targetPanel = targetTab.getAttribute('aria-controls')
+
+  const tabContainer = targetTab.parentNode
+  const mainContainer = tabContainer.parentNode
+
+  tabContainer.querySelector('[aria-selected="true"]').setAttribute('aria-selected', false)
+
+  targetTab.setAttribute('aria-selected', true)
+
+  hideContent(mainContainer, '[role="tabpanel"]')
+  showContent(mainContainer, [`#${targetPanel}`])
+}
+
+function hideContent(parent, content) {
+  parent.querySelectorAll(content).forEach(item => item.setAttribute('hidden', true))
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute('hidden')
+}
