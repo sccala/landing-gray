e4834c658d91c4215d64c69a51bf61100b9d2f59
Chase <sccala18@gmail.com>
2022-03-29 21:34:32 -0700

    mobile nav

---
+++ script.js
@@ -0,0 +1,10 @@
+// @ts-nocheck
+$(function () {
+  $('.carousel').carousel({ interval: 2000 })
+  $('#carouselPause').click(function () {
+    $('.carousel').carousel('pause')
+  })
+  $('#carouselPlay').click(function () {
+    $('.carousel').carousel('cycle')
+  })
+})
